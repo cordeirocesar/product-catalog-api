aef13c6885b249fb9d36a282c2d8201cd46ce74b
Cordeiro Cesar <cesarcordeiromphc@gmail.com>
2026-09-24 10:36:11 +0200

    initial commit

---
+++ src/main/java/com/cordeiro_cesar/product_catalog_api/ProductCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.cordeiro_cesar.product_catalog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogApiApplication.class, args);
+	}
+
+}
