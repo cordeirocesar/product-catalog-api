cbc9fe87c1fcafd40e6edfcf42e0c691d8451401
Cordeiro Cesar <cesarcordeiromphc@gmail.com>
2026-09-24 14:59:49 +0200

    add getAll products method

--- src/main/java/com/cordeiro_cesar/product_catalog_api/controllers/ProductController.java
+++ src/main/java/com/cordeiro_cesar/product_catalog_api/controllers/ProductController.java
@@ -1,15 +1,27 @@
 package com.cordeiro_cesar.product_catalog_api.controllers;
 
+import com.cordeiro_cesar.product_catalog_api.model.Product;
+import com.cordeiro_cesar.product_catalog_api.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    ProductRepository repository;
+
 @GetMapping
 
-    public String ciao(){
-        return "ciao";
+    public ResponseEntity getAll(){
+        List<Product> listproducts = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listproducts);
     }
 }
