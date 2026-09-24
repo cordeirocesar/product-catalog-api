74851f4b7796962ae4cee1fb8876eb4bca300235
Cordeiro Cesar <cesarcordeiromphc@gmail.com>
2026-09-24 16:01:18 +0200

    add product DTO to POST endpoint

--- src/main/java/com/cordeiro_cesar/product_catalog_api/controllers/ProductController.java
+++ src/main/java/com/cordeiro_cesar/product_catalog_api/controllers/ProductController.java
@@ -1,13 +1,13 @@
 package com.cordeiro_cesar.product_catalog_api.controllers;
 
+import com.cordeiro_cesar.product_catalog_api.dtos.ProductsDto;
 import com.cordeiro_cesar.product_catalog_api.model.Product;
 import com.cordeiro_cesar.product_catalog_api.repositories.ProductRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +23,14 @@ public class ProductController {
     public ResponseEntity getAll(){
         List<Product> listproducts = repository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(listproducts);
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ProductsDto dto){
+
+    var product = new Product();
+    BeanUtils.copyProperties(dto,product);
+
+    return   ResponseEntity.status(HttpStatus.CREATED).body(repository.save(product));
     }
 }
---
+++ src/main/java/com/cordeiro_cesar/product_catalog_api/dtos/ProductsDto.java
@@ -0,0 +1,5 @@
+package com.cordeiro_cesar.product_catalog_api.dtos;
+
+public record ProductsDto(String name, Long price) {
+
+}
